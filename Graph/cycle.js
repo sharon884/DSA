f46d56aa14a61b5995c8bf77835e0222cada3e37
sharon884 <sharonsharon9048461583@gmail.com>
2026-01-19 10:57:49 +0530

    implemented the graph data structure once again for understanding the logic of cycle exist or not

--- Graph/cycle.js
+++ Graph/cycle.js
@@ -1,100 +1,185 @@
+// class Graph {
+//     constructor() {
+//         this.adjacencyList = {}
+//     };
+
+
+//     addvertex(v) {
+//         if (!this.adjacencyList[v]) {
+//             this.adjacencyList[v] = [];
+//         };
+//     };
+
+
+//     addEdge(v1, v2) {
+//         if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
+//             return;
+//         };
+
+//         this.adjacencyList[v1].push(v2);
+//         this.adjacencyList[v2].push(v1);
+//     };
+
+
+//     removeEdge(v1, v2) {
+
+//         if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
+//             return;
+//         };
+
+//         this.adjacencyList[v1] = this.adjacencyList[v1].filter((a) => a !== v2);
+//         this.adjacencyList[v2] = this.adjacencyList[v2].filter((a) => a !== v1);
+//     };
+
+//     removeVertex(v1) {
+//         if (!this.adjacencyList[v]) {
+//             return;
+//         };
+
+//         for (let neigh of this.adjacencyList[v1]) {
+//             this.removeEdge(neigh, v1);
+//         };
+
+//         delete this.adjacencyList[v1];
+//     };
+
+
+//     hasCycle() {
+//         let visited = new Set();
+
+//         for (let start in this.adjacencyList) {
+//             if (!visited.has(start)) {
+//                 if (this.bfsCheck(start, visited)) {
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         return false;
+//     };
+
+
+//     bfsCheck(start, visited) {
+//         const queue = [];
+//         visited.add(start);
+//         queue.push([start, null]);
+
+//         while (queue.length > 0) {
+//             const [node, parent] = queue.shift();
+//             for (let neigh of this.adjacencyList[node]) {
+//                 if (!visited.has(neigh)) {
+//                     visited.add(neigh);
+//                     queue.push([neigh, node]);
+//                 } else if (neigh !== parent) {
+
+//                     return true;
+//                 }
+
+
+//             }
+//         };
+
+//         return false;
+//     }
+// };
+
+
+
+// const graph = new Graph();
+// graph.addvertex("A");
+// graph.addvertex("B");
+// graph.addvertex("C");
+// graph.addvertex("D");
+// graph.addEdge("A","B");
+// graph.addEdge("B","C");
+// graph.addEdge("C","B");
+// graph.addEdge("C","D");
+// console.log(graph.hasCycle());
+
+
+
+
 class Graph {
-    constructor() {
-        this.adjacencyList = {}
+    constructor () {
+        this.adajacencyList = {};
     };
 
-
-    addvertex(v) {
-        if (!this.adjacencyList[v]) {
-            this.adjacencyList[v] = [];
+    addVertex( v ) {
+        if ( !this.adajacencyList[v] ) {
+            this.adajacencyList[v] = [];
         };
+
     };
 
 
-    addEdge(v1, v2) {
-        if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
+    addEdge( v1, v2 ) {
+        if ( !this.adajacencyList[v1] || !this.adajacencyList[v2] ) {
             return;
         };
 
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
+        this.adajacencyList[v1].push(v2);
+        this.adajacencyList[v2].push(v1);
     };
 
 
-    removeEdge(v1, v2) {
-
-        if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
+    removeEdge( v1, v2 ) {
+         if ( !this.adajacencyList[v1] || !this.adajacencyList[v2] ) {
             return;
         };
+        this.adajacencyList[v1] = this.adajacencyList[v1].filter((a) => a !== v2);
+        this.adajacencyList[v2] = this.adajacencyList[v2].filter((a) => a !== v1);
 
-        this.adjacencyList[v1] = this.adjacencyList[v1].filter((a) => a !== v2);
-        this.adjacencyList[v2] = this.adjacencyList[v2].filter((a) => a !== v1);
     };
 
-    removeVertex(v1) {
-        if (!this.adjacencyList[v]) {
-            return;
+    removeVertex ( v ) {
+        if ( !this.adajacencyList[v] ) return;
+
+        while ( this.adajacencyList[v].length ) {
+            let neigh = this.adajacencyList[v].pop();
+            this.removeEdge(neigh,v);
         };
 
-        for (let neigh of this.adjacencyList[v1]) {
-            this.removeEdge(neigh, v1);
-        };
-
-        delete this.adjacencyList[v1];
+        delete this.adajacencyList[v];
     };
 
 
     hasCycle() {
         let visited = new Set();
 
-        for (let start in this.adjacencyList) {
-            if (!visited.has(start)) {
-                if (this.bfsCheck(start, visited)) {
+        for ( let vertex in this.adajacencyList ) {
+            if ( !visited.has(vertex) ) {
+                if ( this.bfsCheck( vertex, visited ) ) {
                     return true;
-                }
+                };
             }
-        }
+        };
 
         return false;
     };
 
 
-    bfsCheck(start, visited) {
-        const queue = [];
-        visited.add(start);
-        queue.push([start, null]);
+    bfsCheck( vertex , visited ) {
+        let queue = [];
+        visited.add(vertex);
+         queue.push([vertex, null ]);
 
-        while (queue.length > 0) {
-            const [node, parent] = queue.shift();
-            for (let neigh of this.adjacencyList[node]) {
-                if (!visited.has(neigh)) {
+         while ( queue.length > 0 ) {
+
+            const [ node, parent ] = queue.shift();
+
+            for ( let neigh of this.adajacencyList[node] ) {
+                if ( !visited.has(neigh)) {
                     visited.add(neigh);
-                    queue.push([neigh, node]);
-                } else if (neigh !== parent) {
-
+                    queue.push([neigh,node]);
+                }else if ( neigh != parent ) {
                     return true;
                 }
-
-
             }
-        };
+         };
 
-        return false;
+         return false;
     }
-};
 
 
 
-const graph = new Graph();
-graph.addvertex("A");
-graph.addvertex("B");
-graph.addvertex("C");
-graph.addvertex("D");
-graph.addEdge("A","B");
-graph.addEdge("B","C");
-graph.addEdge("C","B");
-graph.addEdge("C","D");
-console.log(graph.hasCycle());
-
-
-
+} 
