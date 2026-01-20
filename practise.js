7e3e3588643ab4a2c0455efe4a7a727243e7f5b1
sharon884 <sharonsharon9048461583@gmail.com>
2026-01-20 22:27:44 +0530

    implemented the trie data structure with insert, serach , suffix, prefix etc . Next step: implement the autocompletion

--- practise.js
+++ practise.js
@@ -405,3 +405,101 @@
 //         return false;
 //     }
 // }; 
+
+
+
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEnd = false;
+    };
+};
+
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    };
+
+
+    insert ( word ) {
+        let node = this.root;
+        for ( let char of word ) {
+            if ( !node.children[char] ) {
+                node.children[char] = new TrieNode();
+            };
+
+            node = node.children[char];
+        };
+
+        node.isEnd = true;
+    };
+
+
+    search ( word ) {
+        let node = this.root;
+
+        for ( let char of word ) {
+            if (! node.children[char] ) {
+                return false;
+            };
+
+            node = node.children[char];
+        };
+
+        return node.isEnd;
+    };
+
+
+    prefix ( word ) {
+        let node = this.root;
+
+        for ( let char of word ) {
+            if (!node.children[char] ) {
+                return false;
+            };
+
+            node = node.children[char];
+        };
+
+        return  true;
+    };
+
+
+    insertReverse( word ) {
+        let reverse = word.split("").reverse().join("");
+        let node = this.root;
+
+        for ( let char of reverse ) {
+
+             if ( !node.children[char] ) {
+                node.children[char] = new TrieNode();
+             };
+
+             node = node.children[char];
+        
+        }
+            
+        node.isEnd = true;
+    };
+
+
+    suffix( word ) {
+        let node = this.root;
+        let reverse = word.split("").reverse().join("");
+
+        for ( let char of reverse ) {
+            if ( !node.children[char] ) {
+                return false;
+            };
+
+            node = node.children[char];
+        };
+
+        return true;
+        
+    };
+
+
+
+}
