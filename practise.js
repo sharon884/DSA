26d9365bb9f1a2bf779e31a57074690ecf8b46e0
sharon884 <sharonsharon9048461583@gmail.com>
2026-01-21 00:03:55 +0530

    commented whole code for doing practise on tree data structure

--- practise.js
+++ practise.js
@@ -547,85 +547,87 @@
 
 
 
-class MaxHeap {
-    constructor() {
-        this.arr = [];
-    };
+// class MaxHeap {
+//     constructor() {
+//         this.arr = [];
+//     };
 
-    getParent(index) {
-        return Math.floor((index - 1) / 2);
-    };
-    getLeft(index) {
-        return index * 2 + 1
-    };
+//     getParent(index) {
+//         return Math.floor((index - 1) / 2);
+//     };
+//     getLeft(index) {
+//         return index * 2 + 1
+//     };
 
-    getRight(index) {
-        return index * 2 + 2
-    };
+//     getRight(index) {
+//         return index * 2 + 2
+//     };
 
-    swap(i, j) {
-        [this.arr[i], this.arr[j]] = [this.arr[j], this.arr[i]];
-    };
+//     swap(i, j) {
+//         [this.arr[i], this.arr[j]] = [this.arr[j], this.arr[i]];
+//     };
 
-    insert(value) {
-        this.arr.push(value);
-        this.heapifyUp(this.arr.length - 1);
-    };
-
-
-    heapifyUp(index) {
-        while (index > 0) {
-            let parent = this.getParent(index);
-
-            if (this.arr[index] > this.arr[parent]) {
-                this.swap(index, parent);
-                index = parent;
-            } else {
-                break;
-            }
-        }
-    };
+//     insert(value) {
+//         this.arr.push(value);
+//         this.heapifyUp(this.arr.length - 1);
+//     };
 
 
-    extractMax() {
-        if (this.arr.length === 0) return null;
-        if (this.arr.length === 1) {
-            return this.arr.pop();
-        };
+//     heapifyUp(index) {
+//         while (index > 0) {
+//             let parent = this.getParent(index);
+
+//             if (this.arr[index] > this.arr[parent]) {
+//                 this.swap(index, parent);
+//                 index = parent;
+//             } else {
+//                 break;
+//             }
+//         }
+//     };
 
 
-        let max = this.arr[0];
-        this.arr[0] = this.arr.pop();
-        this.heapifyDown(0);
-        return max;
-    };
+//     extractMax() {
+//         if (this.arr.length === 0) return null;
+//         if (this.arr.length === 1) {
+//             return this.arr.pop();
+//         };
 
 
-    heapifyDown(index) {
-        let size = this.arr.length;
+//         let max = this.arr[0];
+//         this.arr[0] = this.arr.pop();
+//         this.heapifyDown(0);
+//         return max;
+//     };
 
 
-        while (true) {
-
-            let largest = index;
-            let left = this.getLeft(index);
-            let right = this.getRight(index);
-
-            if (left < size && this.arr[left] > this.arr[largest]) {
-                largest = left;
-            };
-
-            if (right < size && this.arr[right] > this.arr[largest]) {
-                largest = right;
-            };
+//     heapifyDown(index) {
+//         let size = this.arr.length;
 
 
-            if (largest !== index) {
-                this.swap(largest, index);
-                index = largest;
-            } else {
-                break;
-            }
-        }
-    }
-}
+//         while (true) {
+
+//             let largest = index;
+//             let left = this.getLeft(index);
+//             let right = this.getRight(index);
+
+//             if (left < size && this.arr[left] > this.arr[largest]) {
+//                 largest = left;
+//             };
+
+//             if (right < size && this.arr[right] > this.arr[largest]) {
+//                 largest = right;
+//             };
+
+
+//             if (largest !== index) {
+//                 this.swap(largest, index);
+//                 index = largest;
+//             } else {
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+
